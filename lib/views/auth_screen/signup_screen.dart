62db51b2f2b87a76793a7e81718a87d8a9731393
Vishal Markad <110564423+Vishal7499@users.noreply.github.com>
2024-05-23 01:09:45 +0530

    Add files via upload

---
+++ lib/views/auth_screen/signup_screen.dart
@@ -0,0 +1,142 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/consts/lists.dart';
+import 'package:emart_app/widgets_common/applogo_widget.dart';
+import 'package:emart_app/widgets_common/bg_widget.dart';
+import 'package:emart_app/widgets_common/custom_textfield.dart';
+import 'package:emart_app/widgets_common/our_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  bool isCheck = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            children: [
+              (context.screenHeight * 0.1).heightBox,
+              applogoWidget(),
+              10.heightBox,
+              "Join the $appname".text.fontFamily(bold).white.size(18).make(),
+              15.heightBox,
+              Column(
+                children: [
+                  customTextField(hint: nameHint, title: name),
+                  customTextField(hint: emailHint, title: email),
+                  customTextField(hint: passwordHint, title: password),
+                  customTextField(hint: passwordHint, title: rertypePassword),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: forgotPass.text.make(),
+                    ),
+                  ),
+                  5.heightBox,
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isCheck,
+                        onChanged: (newValue) {
+                          setState(() {
+                            isCheck = newValue!;
+                          });
+                        },
+                        activeColor: isCheck ? whiteColor : redColor,
+                        fillColor: MaterialStateProperty.resolveWith((states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return redColor;
+                          }
+                          return Colors.transparent;
+                        }),
+                        checkColor: whiteColor,
+                      ),
+                      10.widthBox,
+                      Expanded(
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "I agree to the ",
+                                style: TextStyle(
+                                  fontFamily: regular,
+                                  color: fontGrey,
+                                ),
+                              ),
+                              TextSpan(
+                                text: termsAndCondi,
+                                style: TextStyle(
+                                  fontFamily: regular,
+                                  color: redColor,
+                                ),
+                              ),
+                              TextSpan(
+                                text: " & ",
+                                style: TextStyle(
+                                  fontFamily: regular,
+                                  color: fontGrey,
+                                ),
+                              ),
+                              TextSpan(
+                                text: privacyPolicy,
+                                style: TextStyle(
+                                  fontFamily: regular,
+                                  color: redColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  5.heightBox,
+                  ourButton(
+                    color: isCheck==true? redColor:lightGrey,
+                    title: signup,
+                    textColor: whiteColor,
+                    onPress: () {},
+                  ).box.width(context.screenWidth - 50).make(),
+                  10.heightBox,
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: alreadyHaveAccount,
+                          style: TextStyle(
+                            fontFamily: bold,
+                            color: fontGrey,
+                          ),
+                        ),
+                        TextSpan(
+                          text: login,
+                          style: TextStyle(
+                            fontFamily: bold,
+                            color: redColor,
+                          ),
+                        )
+                      ],
+                    ),
+                  ).onTap(() {
+                    Get.back();
+                  }),
+                ],
+              ).box.white.rounded.padding(const EdgeInsets.all(16)).width(context.screenWidth - 70).shadowSm.make(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
